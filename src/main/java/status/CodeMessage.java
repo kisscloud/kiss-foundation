407c7216a67ce5738cd150210f45d62a9af2029e
qianrongli <qianrongli@uchain.one>
2018-12-06 09:46:20 +0800

    modify muti language

--- src/main/java/status/CodeMessage.java
+++ src/main/java/status/CodeMessage.java
@@ -26,8 +26,6 @@ public class CodeMessage {
 
         ThreadLocalUtil.setString(MessageResource.I18N_ATTRIBUTE,"enums");
         String language = ApplicationUtil.getHttpServletRequest().getHeader("X-LANGUAGE");
-        System.out.println("=====1====" + language);
-        System.out.println(new Locale(language == null ? "zh_cn":language.replace("-","_")).getLanguage());
         String message = messageSource.getMessage(key + option, null, new Locale(language == null ? "zh_cn":language.replace("-","_")));
 
         return message;
